bf065b9462bb8e7586ffe8854e27e01822f806d2
dawnbanawn <danheikenberg@hotmail.com>
2023-05-14 11:06:51 +0200

    Page 4

--- Project_09_Robericano/script.js
+++ Project_09_Robericano/script.js
@@ -2,6 +2,9 @@ const buttonPage01 = document.querySelector("#button01");
 const buttonPage02 = document.querySelector("#button02");
 const buttonPage03 = document.querySelector("#button03");
 const buttonPage03Back = document.querySelector("#button04");
+const button04back = document.querySelector("#button04back");
+const button04next = document.querySelector("#button04next");
+
 const gameTimeField = document.querySelector("#gameTime");
 let gameTime;
 page02Container = document.querySelector("#page02Container");
@@ -11,7 +14,14 @@ const nameInputContainer = document.querySelector("#nameInputContainer");
 const playerNumberDropdown = document.querySelector("#numberOfPlayersDropDown");
 let numberOfPlayers;
 let playerNamesArray = [];
-
+button04back.addEventListener("click", () => {
+  page04Container.style.display = "none";
+  page03Container.style.display = "block";
+});
+buttonPage03.addEventListener("click", () => {
+  page03Container.style.display = "none";
+  page04Container.style.display = "block";
+});
 buttonPage03Back.addEventListener("click", () => {
   page03Container.style.display = "none";
   page02Container.style.display = "block";
@@ -32,8 +42,6 @@ buttonPage01.addEventListener("click", () => {
   //buttonPage02.style.backgroundColor  = "black";
 });
 
-
-
 buttonPage02.addEventListener("click", () => {
   if (gameTimeField.value == "") {
     gameTime = 120;
@@ -53,6 +61,17 @@ buttonPage02.addEventListener("click", () => {
     //window.setTimeout(loadNameInputs, 2000);
     //window.open("./page03.html","_self");
     //window.location.href = './page03.html';
+    let page03chosenGameTimeSpan = document.querySelector(
+      "#page03chosenGameTimeSpan"
+    );
+
+    page03chosenGameTimeSpan.innerHTML = gameTime;
+    let page03PlayerNumberSpan = document.querySelector(
+      "#page03PlayerNumberSpan"
+    );
+
+    page03PlayerNumberSpan.innerHTML = numberOfPlayers;
+
     page02Container.style.display = "none";
     page03Container.style.display = "block";
     loadNameInputs();
@@ -68,15 +87,14 @@ function loadNameInputs() {
 
     inputElement.required;
     inputElement.minlength = "0";
-    inputElement.maxlength = "10";
-    if (playerNamesArray[i - 1] != "" && playerNamesArray[i - 1] != undefined ) {
+    inputElement.maxlength = "14";
+    if (playerNamesArray[i - 1] != "" && playerNamesArray[i - 1] != undefined) {
       inputElement.value = playerNamesArray[i - 1];
     }
     nameInputContainer.appendChild(inputElement);
     //console.log("page03" + numberOfPlayers);
   }
 }
-
 
 //function saveNames(numberOfPlayers) {
 
