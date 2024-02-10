64a0d7fa5283f08f564edaa1d66bfc6f86d3ad82
dawnbanawn <danheikenberg@hotmail.com>
2024-02-10 22:22:14 +0100

    html with navbar

--- Project_17_Python Website/website/views.py
+++ Project_17_Python Website/website/views.py
@@ -1,9 +1,9 @@
 # routes
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 # Define route / view
 @views.route('/')
 def home():
-    return "<h1>test</h1>"
+    return render_template("home.html")
